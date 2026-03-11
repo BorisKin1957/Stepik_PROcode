bf72c467a69154d92bbe794173cf3f71cfcf7872
BorisKin1957 <6063354@gmail.com>
2026-03-11 12:55:05 +0700

    11.8 Использование и создание декораторов функций

--- temp.py
+++ temp.py
@@ -1,1 +1,18 @@
-print(84 % 120)
+def my_decorator(func):
+    def w(*args, **kwargs):
+        print("Перед вызовом функции")
+        #r = func(*args, **kwargs)
+        print("После вызова функции")
+        return func(*args, **kwargs)
+    return w
+@my_decorator
+def say_hello(a, d):
+    print("Привет!")
+    return a + d
+
+# Применение декоратора
+#decorated_say_hello = my_decorator(say_hello)
+
+# Вызов декорированной функции
+
+print(say_hello(1, 2))
